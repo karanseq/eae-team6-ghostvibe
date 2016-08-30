06661695dcc012b1f74f228db6ece67cbd55723e
u1087117 <u1087117@eae-d-110>
2016-08-30 15:36:32 -0600

    Added more actions, added a class to handle haptic feedback and fixed a few bugs in the scheduler.

---
+++ GhostVibe/GhostVibe/Helper/HapticFeedback.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Helper
+{
+    class HapticFeedback
+    {
+        protected static UpdateDelegate delegateStartVibration = new UpdateDelegate(HapticFeedback.startVibration);
+        protected static UpdateDelegate delegateStopVibration = new UpdateDelegate(HapticFeedback.stopVibration);
+        protected static float beatIntensity = 0.25f;
+
+        private HapticFeedback() { }
+
+        protected static void startVibration(float deltaTime)
+        {
+            GamePad.SetVibration(PlayerIndex.One, HapticFeedback.beatIntensity, HapticFeedback.beatIntensity);
+        }
+
+        protected static void stopVibration(float deltaTime)
+        {
+            GamePad.SetVibration(PlayerIndex.One, 0.0f, 0.0f);
+        }
+
+        public static void startBeats(float beatFrequency, float beatDuration, float beatIntensity)
+        {
+            HapticFeedback.beatIntensity = beatIntensity;
+            Scheduler.Instance.scheduleDelegate(delegateStartVibration, beatFrequency, Timer.RepeatForever, 0.0f);
+            Scheduler.Instance.scheduleDelegate(delegateStopVibration, beatFrequency, Timer.RepeatForever, beatDuration);
+        }
+
+        public static void stopBeats()
+        {
+            Scheduler.Instance.unscheduleDelegate(delegateStartVibration);
+            Scheduler.Instance.unscheduleDelegate(delegateStopVibration);
+            stopVibration(0.0f);
+        }
+    }
+}
